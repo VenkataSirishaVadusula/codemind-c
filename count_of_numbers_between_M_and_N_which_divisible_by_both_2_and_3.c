26dc074607d0297346d2242f6a976becd25ca213
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-07 11:27:24 +0530

    #include<stdio.h> int count(int M,int N,int i,int c); int main() {     int M,N,i,c=0;     scanf("%d%d",&M,&N);     int x=count(M,N,i,c); } int count(int M,int N,int i,int c) {     for(i=M;i<=N;i++)     {         if(i%2==0 && i%3==0)         {             c++;         }     }     printf("%d",c);     return 0; }

--- count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
+++ count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int count(int M,int N,int i,int c);
 int main()
 {
     int M,N,i,c=0;
     scanf("%d%d",&M,&N);
+    int x=count(M,N,i,c);
+}
+int count(int M,int N,int i,int c)
+{
     for(i=M;i<=N;i++)
     {
         if(i%2==0 && i%3==0)
@@ -11,4 +16,5 @@ int main()
         }
     }
     printf("%d",c);
+    return 0;
 }
