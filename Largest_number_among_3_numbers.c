393e24502f8f4f75727e4c62cd91dc5fb7c3bbf0
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-07 10:18:18 +0530

    #include<stdio.h> int large(int a,int b,int c); int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int x=large(a,b,c); } int large(int a,int b,int c) {     if(a>c && a>b)     {         printf("%d",a);     }     else     {         printf("%d",c);     }     return 0; }

--- Largest_number_among_3_numbers.c
+++ Largest_number_among_3_numbers.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int large(int a,int b,int c);
 int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
+    int x=large(a,b,c);
+}
+int large(int a,int b,int c)
+{
     if(a>c && a>b)
     {
         printf("%d",a);
@@ -11,4 +16,5 @@ int main()
     {
         printf("%d",c);
     }
+    return 0;
 }
