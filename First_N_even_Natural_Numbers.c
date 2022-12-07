4f46985d0dcb3da0db19ae687cb3b2e0fcee258f
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-07 11:38:58 +0530

    #include<stdio.h> int even(int i,int n); int main() {     int i,n;     scanf("%d",&n);     int x=even(i,n); } int even(int i,int n) {     for(i=n*2;i>=1;i--)     {         if(i%2==0)         {             printf("%d ",i);         }     }     return 0; }

--- First_N_even_Natural_Numbers.c
+++ First_N_even_Natural_Numbers.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int even(int i,int n);
 int main()
 {
     int i,n;
     scanf("%d",&n);
+    int x=even(i,n);
+}
+int even(int i,int n)
+{
     for(i=n*2;i>=1;i--)
     {
         if(i%2==0)
@@ -10,4 +15,5 @@ int main()
             printf("%d ",i);
         }
     }
+    return 0;
 }
