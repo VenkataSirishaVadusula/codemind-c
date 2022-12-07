867f962409a388028b0d10f54f66969a7bf9a251
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-07 11:34:31 +0530

    #include<stdio.h> int count(int n,int m,int i,int c); int main() {     int n,m,i,c=0;     scanf("%d%d",&m,&n);     int x=count(n,m,i,c); } int count(int n,int m,int i,int c) {     for(i=m;i<=n;i++)     {        if(i%3==0)         {             c++;         }     }     printf("%d",c);     return 0; }

--- count_Multiples_of_3_between_M_,N.c
+++ count_Multiples_of_3_between_M_,N.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int count(int n,int m,int i,int c);
 int main()
 {
     int n,m,i,c=0;
     scanf("%d%d",&m,&n);
+    int x=count(n,m,i,c);
+}
+int count(int n,int m,int i,int c)
+{
     for(i=m;i<=n;i++)
     {
        if(i%3==0)
@@ -11,4 +16,5 @@ int main()
         }
     }
     printf("%d",c);
+    return 0;
 }
