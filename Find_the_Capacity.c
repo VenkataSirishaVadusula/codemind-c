374019fa165a26d5341f86667184e38ba8d5fb01
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-12 11:17:07 +0530

    #include<stdio.h> int capa(int cap,int s,int t,int b,int tc); int main() {     int cap,s,t,b,tc;     scanf("%d%d%d",&s,&t,&b);     int x=capa(cap,s,t,b,tc); } int capa(int cap,int s,int t,int b,int tc) {     cap=2*s*t*b*512;     tc=cap/1024;     printf("%dKB",tc);     return 0; }

--- Find_the_Capacity.c
+++ Find_the_Capacity.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+int capa(int cap,int s,int t,int b,int tc);
 int main()
 {
     int cap,s,t,b,tc;
     scanf("%d%d%d",&s,&t,&b);
+    int x=capa(cap,s,t,b,tc);
+}
+int capa(int cap,int s,int t,int b,int tc)
+{
     cap=2*s*t*b*512;
     tc=cap/1024;
     printf("%dKB",tc);
+    return 0;
 }
