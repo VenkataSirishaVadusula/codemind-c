1a0926b0f291727cff7a80b9dd523469ef00b25f
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-12 10:44:10 +0530

    #include<stdio.h> int mario(int X); int main() {     int X;     scanf("%d",&X);     int x=mario(X); } int mario(int X) {     if(X%3==1)     printf("HUGE");     else if(X%3==0)     printf("NORMAL");     else     printf("SMALL");     return 0; }

--- Mario_and_Mushrooms.c
+++ Mario_and_Mushrooms.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
+int mario(int X);
 int main()
 {
     int X;
     scanf("%d",&X);
+    int x=mario(X);
+}
+int mario(int X)
+{
     if(X%3==1)
     printf("HUGE");
     else if(X%3==0)
     printf("NORMAL");
     else
     printf("SMALL");
+    return 0;
 }
