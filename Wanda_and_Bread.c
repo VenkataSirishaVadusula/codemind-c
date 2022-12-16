c8c6ca68ebf356f8d273b4041ca493343b416b3d
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-16 19:59:21 +0530

    #include<stdio.h> int bread(int n,int m,int k); int main() {     int n,m,k;     scanf("%d%d%d",&n,&m,&k);     int x=bread(n,m,k); } int bread(int n,int m,int k) {     if(m*k<n)     printf("NO");     else     printf("YES");     return 0; }

--- Wanda_and_Bread.c
+++ Wanda_and_Bread.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+int bread(int n,int m,int k);
 int main()
 {
     int n,m,k;
     scanf("%d%d%d",&n,&m,&k);
+    int x=bread(n,m,k);
+}
+int bread(int n,int m,int k)
+{
     if(m*k<n)
     printf("NO");
     else
     printf("YES");
+    return 0;
 }
