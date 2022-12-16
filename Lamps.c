b1ba103b005bea53e04ef33dea4a09eea50fd7f6
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-16 19:55:20 +0530

    #include<stdio.h> int lamps(int n,int k,int x,int y); int main() {     int n,k,x,y;     scanf("%d%d%d%d",&n,&k,&x,&y);     int s=lamps(n,k,x,y); } int lamps(int n,int k,int x,int y) {     if(x<y)     {         printf("%d",n*x);     }     else     {         printf("%d",k*x+(n-k)*y);     }     return 0; }

--- Lamps.c
+++ Lamps.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int lamps(int n,int k,int x,int y);
 int main()
 {
     int n,k,x,y;
     scanf("%d%d%d%d",&n,&k,&x,&y);
+    int s=lamps(n,k,x,y);
+}
+int lamps(int n,int k,int x,int y)
+{
     if(x<y)
     {
         printf("%d",n*x);
@@ -11,4 +16,5 @@ int main()
     {
         printf("%d",k*x+(n-k)*y);
     }
+    return 0;
 }
