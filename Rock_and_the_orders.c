129691f066d1403c01ad67b6eb87784d1650f46d
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-16 19:40:29 +0530

    #include<stdio.h> void rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y); int main() {     int d,c,a1,a2,a3,b1,b2,b3,x,y;     scanf("%d%d",&d,&c);     scanf("%d%d%d%d%d%d",&a1,&a2,&a3,&b1,&b2,&b3);     rock(d,c,a1,a2,a3,b1,b2,b3,x,y); } void rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y) {     x=a1+a2+a3;     y=b1+b2+b3;     if(x<150&&y<150)     {         printf("NO");     }     else if(x>=150&&y>=150)     {         if(x+y+2*d>x+y+c)         {             printf("YES");         }         else         {             printf("NO");         }     }     else if(x<150&&y>=150)     {         if(x+y+2*d>x+y+c)         {             printf("YES");         }         else         {             printf("NO");         }     }     else if(x<150&& y>=150)     {         if(x+y+2*d>x+y+c)         {             printf("YES");         }         else         {             printf("NO");         }     }     else if(x>=150&& y<150)     {         if(x+y+d>x+y+c)         {             printf("YES");         }         else         {             printf("NO");         }     } }

--- Rock_and_the_orders.c
+++ Rock_and_the_orders.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-int rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y);
+void rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y);
 int main()
 {
     int d,c,a1,a2,a3,b1,b2,b3,x,y;
     scanf("%d%d",&d,&c);
     scanf("%d%d%d%d%d%d",&a1,&a2,&a3,&b1,&b2,&b3);
-    int s=rock(d,c,a1,a2,a3,b1,b2,b3,x,y);
+    rock(d,c,a1,a2,a3,b1,b2,b3,x,y);
 }
-int rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y)
+void rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y)
 {
     x=a1+a2+a3;
     y=b1+b2+b3;
@@ -59,5 +59,4 @@ int rock(int d,int c,int a1,int a2,int a3,int b1,int b2,int b3,int x,int y)
             printf("NO");
         }
     }
-    return 0;
 }
