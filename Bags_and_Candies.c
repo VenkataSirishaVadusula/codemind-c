2f2d45683ae983e01272fdc76cd468fc06f86d16
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-21 21:33:51 +0530

    #include<stdio.h> void bags(int n,int k,int m); int main() {     int n,k,m;     scanf("%d%d%d",&n,&k,&m);     bags(n,k,m); } void bags(int n,int k,int m) {     int cpb=m*k;     if(n%cpb==0)     {         printf("%d",n/cpb);     }     else     {         printf("%d",n/cpb+1);     } }

--- Bags_and_Candies.c
+++ Bags_and_Candies.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int bags(int n,int k,int m);
+void bags(int n,int k,int m);
 int main()
 {
     int n,k,m;
     scanf("%d%d%d",&n,&k,&m);
-    int x=bags(n,k,m);
+    bags(n,k,m);
 }
-int bags(int n,int k,int m)
+void bags(int n,int k,int m)
 {
     int cpb=m*k;
     if(n%cpb==0)
@@ -17,5 +17,4 @@ int bags(int n,int k,int m)
     {
         printf("%d",n/cpb+1);
     }
-   return 0; 
 }
