5ea19fc40b075f0cedeb7bbb30ae77d87335e6e5
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-28 21:29:44 +0530

    #include<stdio.h> void fibo(int a,int b,int c,int n); int main() {     int a=0,b=1,c=0,n;     scanf("%d",&n);     fibo(a,b,c,n); } void fibo(int a,int b,int c,int n) {     while(n>c)     {         c=a+b;         a=b;         b=c;     }     if(n-a<b-n)     {         printf("%d ",a);     }     else if(n-a==b-n)     {         printf("%d %d",a,b);     }     else     {         printf("%d ",b);     } }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int fibo(int a,int b,int c,int n);
+void fibo(int a,int b,int c,int n);
 int main()
 {
     int a=0,b=1,c=0,n;
     scanf("%d",&n);
-    int x=fibo(a,b,c,n);
+    fibo(a,b,c,n);
 }
-int fibo(int a,int b,int c,int n)
+void fibo(int a,int b,int c,int n)
 {
     while(n>c)
     {
@@ -26,5 +26,4 @@ int fibo(int a,int b,int c,int n)
     {
         printf("%d ",b);
     }
-    return 0;
 }
