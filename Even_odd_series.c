8210a65fdfd144c7c0f4c931e9796530e29f7ef1
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-29 22:45:14 +0530

    #include<stdio.h> #include<math.h> int eos(int i,int n,int a,int b,int k1,int k2); int main() {     int i,n,a,b,k1=0,k2=1;     scanf("%d",&n);     int x=eos(i,n,a,b,k1,k2); } int eos(int i,int n,int a,int b,int k1,int k2) {     for(i=1;i<=n+1;i++)     {         if(i%2==0)         {             a=pow(3,k1-1);             printf("%d ",a);             k1++;         }         else         {             b=pow(2,k2);             printf("%d ",b);             k2++;         }     }     return 0; }

--- Even_odd_series.c
+++ Even_odd_series.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<math.h>
+int eos(int i,int n,int a,int b,int k1,int k2);
 int main()
 {
     int i,n,a,b,k1=0,k2=1;
     scanf("%d",&n);
+    int x=eos(i,n,a,b,k1,k2);
+}
+int eos(int i,int n,int a,int b,int k1,int k2)
+{
     for(i=1;i<=n+1;i++)
     {
         if(i%2==0)
@@ -19,4 +24,5 @@ int main()
             k2++;  
         }
     }
+    return 0;
 }
