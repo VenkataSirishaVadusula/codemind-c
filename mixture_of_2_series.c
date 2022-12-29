d409ef975885f9bd4bbada83cd3a130a2c76d489
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-29 23:13:51 +0530

    #include<stdio.h> int mix(int i,int n,int k,int a,int b); int main() {     int i,n,k=0,a,b=0;     scanf("%d",&n);     int x=mix(i,n,k,a,b); } int mix(int i,int n,int k,int a,int b) {     for(i=1;i<=n+1;i++)     {         if(i%2==0)         {             printf("%d ",b);             b++;         }         else         {             a=k*2;             printf("%d ",a);             k++;         }     }     return 0; }

--- mixture_of_2_series.c
+++ mixture_of_2_series.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int mix(int i,int n,int k,int a,int b);
 int main()
 {
     int i,n,k=0,a,b=0;
     scanf("%d",&n);
+    int x=mix(i,n,k,a,b);
+}
+int mix(int i,int n,int k,int a,int b)
+{
     for(i=1;i<=n+1;i++)
     {
         if(i%2==0)
@@ -17,4 +22,5 @@ int main()
             k++;
         }
     }
+    return 0;
 }
