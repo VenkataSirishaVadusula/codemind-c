7d6d3ec5c60befe5982a4a919a1e27058bc1e419
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2022-12-29 17:44:04 +0530

    #include<stdio.h> int tour(int t,int n,int m); int main() {     int t,n,m;     scanf("%d",&t);     int x=tour(t,n,m); } int tour(int t,int n,int m) {     while(t)     {         scanf("%d%d",&n,&m);         printf("%d ",n*5+m*7);         t--;     }     return 0; }

--- Tour.c
+++ Tour.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int tour(int t,int n,int m);
 int main()
 {
     int t,n,m;
     scanf("%d",&t);
+    int x=tour(t,n,m);
+}
+int tour(int t,int n,int m)
+{
     while(t)
     {
         scanf("%d%d",&n,&m);
@@ -10,4 +15,5 @@ int main()
 ",n*5+m*7);
         t--;
     }
+    return 0;
 }
