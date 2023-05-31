3556f46f529ed07ed62f0da1c63367a5933035b4
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2023-05-31 09:09:49 +0530

    #include<stdio.h> int digicnt(int n) {     int r,k=0;     while(n>0)     {         r=n%10;         k++;         n=n/10;     }     return k; } int main() {     int n;     scanf("%d",&n);     int k=digicnt(n);     int x[k];     for(int i=k-1;i>=0;i--)     {         x[i]=n%10;         n=n/10;     }     for(int i=0;i<k;i++)     {         if(x[i]!=9)         {             x[i]=9;             break;         }     }     for(int i=0;i<k;i++)     {         printf("%d",x[i]);     } }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,28 +1,36 @@
 #include<stdio.h>
+int digicnt(int n)
+{
+    int r,k=0;
+    while(n>0)
+    {
+        r=n%10;
+        k++;
+        n=n/10;
+    }
+    return k;
+}
 int main()
 {
-    int n,d,a[100],i,j;
+    int n;
     scanf("%d",&n);
-    i=0;
-    while(n!=0)
+    int k=digicnt(n);
+    int x[k];
+    for(int i=k-1;i>=0;i--)
     {
-        d=n%10;
+        x[i]=n%10;
         n=n/10;
-        a[i]=d;
-        i++;
     }
-    for(j=i-1;j>=0;j--)
+    for(int i=0;i<k;i++)
     {
+        if(x[i]!=9)
         {
-            if(a[j]==6)
-            {
-                a[j]=9;
-                break;
-            }
+            x[i]=9;
+            break;
         }
     }
-    for(j=i-1;j>=0;j--)
+    for(int i=0;i<k;i++)
     {
-        printf("%d",a[j]);
+        printf("%d",x[i]);
     }
 }
