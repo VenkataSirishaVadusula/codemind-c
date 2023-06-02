066cdda3b0e2afdc304407130dae36b8b05d7944
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2023-06-02 15:30:35 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        scanf("%d",&n);
        int x[n];
        for(int i=0;i<n;i++)
        {
            scanf("%d",&x[i]);
        }
        int c=0,s=0;
        for(int i=0;i<n;i++)
        {
            c=1;
            for(int j=0;j<n;j++)
            {
                    if(i!=j&&x[i]==x[j])
                    {
                        c++;
                        x[j]=0;
                    }
            }
            if(c==x[i])
            {
                s++;
            }
        }
        printf("%d",s);
    }

---
+++ value_same_as_count.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    int c=0,s=0;
+    for(int i=0;i<n;i++)
+    {
+        c=1;
+        for(int j=0;j<n;j++)
+        {
+                if(i!=j&&x[i]==x[j])
+                {
+                    c++;
+                    x[j]=0;
+                }
+        }
+        if(c==x[i])
+        {
+            s++;
+        }
+    }
+    printf("%d",s);
+}
